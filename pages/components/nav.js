2d3dadda10b2d9da45b4eac672939d71329cf80e
sanbleb_bash <sanbleb9@hotmail.com>
2022-08-11 19:32:59 +0200

    edited the nav and animation in the home page

--- pages/components/nav.js
+++ pages/components/nav.js
@@ -5,13 +5,16 @@ const Navbar = () => {
 		<nav className=' w-screen bg-slate-100 h-[10vh] font-fila '>
 			<div className='mx-auto h-full container flex items-center justify-between text-gray-500  '>
 				<ul className='flex text-2xl items-end gap-3'>
-					<h1 className=' font-semibold text-gray-700 text-2xl md:text-4xl pr-10 capitalize'>
-						nino-multimedia
+					<h1 className=' font-semibold text-gray-600 text-2xl md:text-4xl pr-10 '>
+						san Bleb<span className='text-red-500 capitalize '>.dev</span>
 					</h1>
 
 					<li className='mr-3'>about</li>
 					<li className='mr-3'>my work</li>
 				</ul>
+				<button className=' border px-5 py-2 rounded-md text-purple-400 shadow-md hover:bg-purple-100 hover:border hover:text-gray-500 border-white text-inherit transition-all ease-out duration-100 delay-75 '>
+					hire me
+				</button>
 			</div>
 		</nav>
 	);
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,64 @@ module.exports = {
 				fila: ['Fira Code', 'monospace'],
 				poppins: ['Poppins', ' sans-serif'],
 			},
+			animation: {
+				'fade-in': 'fadeLeft 1s ease-in forwards',
+				'fade-out': 'fadeRight 1s ease-in forewards',
+				'draw-top': 'drawTop 10s ease-in-out alternate  infinite',
+				'fade-leters': 'fadeLetters 1s ease-in-out alternate  infinite',
+			},
+			keyframes: {
+				fadeLeft: {
+					'0%': {
+						opacity: 0,
+						transform: 'translateX(-30%)',
+					},
+					'50%': {
+						opacity: 0.5,
+					},
+					'100%': {
+						opacity: 1,
+						transform: 'translateX(1)',
+					},
+				},
+
+				fadeRight: {
+					'0%': {
+						opacity: 0,
+						transform: 'translateX(120%)',
+					},
+					'50%': {
+						opacity: 0.5,
+						transform: 'translateX(30%)',
+					},
+					'100%': {
+						opacity: 1,
+						transform: 'translateX(1)',
+					},
+				},
+
+				drawTop: {
+					'0%': {
+						transform: 'translateY(0)',
+					},
+					'50%': {
+						transform: 'translateY(-55px)',
+					},
+
+					'100%': {
+						transform: 'translateY(-110px)',
+					},
+				},
+
+				fadeLetters: {
+					'0%': {
+						opacity: 0.5,
+					},
+					'100%': {
+						opacity: 1,
+					},
+				},
+			},
 		},
 	},
 	plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
