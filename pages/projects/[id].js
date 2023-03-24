bb391cac58190627308289c2a1c860bb650bd757
sanbleb_bash <sanbleb9@hotmail.com>
2023-03-24 15:29:01 +0200

    fixed the layout for the image

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -11,7 +11,8 @@ const DetailsPage = () => {
 
 	const router = useRouter();
 	const title = router.asPath.split('/')[2];
-	const project = data.find((project) => project.name === title);
+	const altTitle = title.replaceAll('%20', ' ');
+	const project = data.find((project) => project.name === altTitle);
 
 	const sliderImages = project?.image;
 	const handleNext = (direction) => {
@@ -22,68 +23,59 @@ const DetailsPage = () => {
 		}
 	};
 
-	if (!project) {
-		return (
-			<h1 className=' text-gray-600 text-center '>sorry thats not available</h1>
-		);
-	}
-
 	return (
 		<div className='w-[80vw] pt-[10vh] px-4 min-h-[80vh] mx-auto '>
-			<h1 className=' text-gray-600 pb-7 '>{title} app</h1>
+			{!project ? (
+				<h1 className='pt-[10vh] text-gray-600 text-center '>
+					sorry thats was`t available
+				</h1>
+			) : (
+				<>
+					<h1 className=' text-gray-600 pb-7 '>{altTitle} app</h1>
 
-			<div className='flex  justify-between items-center w-full py-6 '>
-				<div className='w-full h-full '>
-					<div className='w-full  p-4 relative h-[40vh] shadow-xl shadow-red-200 mb-7 flex items-center '>
-						{sliderImages?.map((image, index) => (
-							<Image
-								key={index}
-								src={image}
-								alt={project.name}
-								layout='fill'
-								objectFit='cover'
-								className='rounded-lg w-full  object-fill shadow-lg '
-							/>
-						))}
-					</div>
-					{sliderImages.length > 1 && (
-						<span className='absolute top-0 left-0  w-full h-full flex justify-between items-center '>
-							<AiOutlineArrowLeft
-								onClick={(left) => handleNext(left)}
-								className='bg-purple-400 text-white rounded-full text-4xl cursor-pointer ml-14  '
-							/>
-							<AiOutlineArrowRight
-								onClick={(right) => handleNext(right)}
-								className='bg-purple-400 text-white rounded-full text-4xl mr-14 cursor-pointer  '
-							/>
-						</span>
-					)}
-					<div className='bg-white shadow-md rounded-lg p-4'>
-						<div className='flex justify-between items-start gap-4 flex-col '>
-							<h2 className='text-2xl font-bold'>{project.name}</h2>
-							<h2 className='  tracking-widest bg-purple-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-								{project.techStack}
-							</h2>
-							<div className='flex'>
-								<p className='bg-gray-200 rounded-md leading-7  px-3 py-1 text-sm tracking-wider font-semibold text-gray-700 mr-2'>
-									{project.description}
-								</p>
+					<div className=' w-full flex  justify-between items-center  py-6 '>
+						<div className='w-full min-h-screen  p-2 '>
+							<div className='w-full  p-2 relative min-h-[80vh] shadow-lg  py-7 flex items-center '>
+								{sliderImages?.map((image, index) => (
+									<Image
+										key={index}
+										src={image}
+										alt={project.name}
+										layout='fill'
+										objectFit='cover'
+										className='rounded-lg w-full h-full  object-cover shadow-lg '
+									/>
+								))}
 							</div>
-							<a
-								rel='nooperner noreferrer'
-								href={project.link}
-								target='_blank'
-								className='bg-purple-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'
-							>
-								visit site
-							</a>
+
+							<div className='bg-white shadow-md rounded-lg p-4'>
+								<div className='flex justify-between items-start gap-4 flex-col '>
+									<h2 className='text-2xl font-bold'>{project.name}</h2>
+									<h2 className='  tracking-widest bg-purple-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
+										{project.techStack}
+									</h2>
+									<div className='flex'>
+										<p className='bg-gray-200 rounded-md leading-7  px-3 py-1 text-sm tracking-wider font-semibold text-gray-700 mr-2'>
+											{project.description}
+										</p>
+									</div>
+									<a
+										rel='nooperner noreferrer'
+										href={project.link}
+										target='_blank'
+										className='bg-purple-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'
+									>
+										visit site
+									</a>
+								</div>
+							</div>
 						</div>
 					</div>
-				</div>
-			</div>
-			<Link href='/projects' className='cursor-pointer bg-purple-300 m-2'>
-				go back
-			</Link>
+					<Link href='/projects' className='cursor-pointer bg-purple-300 m-2'>
+						go back
+					</Link>
+				</>
+			)}
 		</div>
 	);
 };
